48f27337f6021d0942e8bd2b286c9e68d0fab8f2
Mikazuki <44475022+3rdtsuki@users.noreply.github.com>
2021-12-01 13:46:40 +0800

    Update server.cpp

--- 基本完善的双人通信/server.cpp
+++ 基本完善的双人通信/server.cpp
@@ -21,117 +21,117 @@ LIST_STRING::iterator user;
 
 
 int send_to(SOCKET socket,string s){
-	const char *c=s.c_str();
-	return send(socket,c,200,0);
+    const char *c=s.c_str();
+    return send(socket,c,200,0);
 }
 
 string recv_from(SOCKET socket){
-	char c[200];
-	recv(socket,c,200,0);
-	string s=c;
-	return s;
+    char c[200];
+    recv(socket,c,200,0);
+    string s=c;
+    return s;
 }
 
 string show_online_users(){
-	string sendbuf="当前在线用户：";
-	for(user=users.begin();user!=users.end();++user){
-		sendbuf+=*user+' ';
-	}
-	return sendbuf;
+    string sendbuf="当前在线用户：";
+    for(user=users.begin();user!=users.end();++user){
+        sendbuf+=*user+' ';
+    }
+    return sendbuf;
 }
 
 bool user_is_online(string name){
-	for(user=users.begin();user!=users.end();++user){
-		if(*user==name){
-			break;
-		}
-	}
-	if(user==users.end())return false;
-	return true;
+    for(user=users.begin();user!=users.end();++user){
+        if(*user==name){
+            break;
+        }
+    }
+    if(user==users.end())return false;
+    return true;
 }
 int main(){
-	WSADATA wsaData;
-	WORD wVersionRequested = MAKEWORD(2,1); 
-	WSAStartup(wVersionRequested, &wsaData);
-	
-	SOCKET sockSrv=socket(AF_INET,SOCK_STREAM,0);//创建服务器端socket
-	struct sockaddr_in server_addr;//服务器端地址
-	int addrlen=sizeof(server_addr);
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(SERVER_PORT);//服务器端口号定为6666
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	bind(sockSrv,(SOCKADDR*)&server_addr,sizeof(SOCKADDR));//套接字绑定服务器端地址
-	listen(sockSrv,5);//队列的最大长度为5
+    WSADATA wsaData;
+    WORD wVersionRequested = MAKEWORD(2,1); 
+    WSAStartup(wVersionRequested, &wsaData);
+    
+    SOCKET sockSrv=socket(AF_INET,SOCK_STREAM,0);//创建服务器端socket
+    struct sockaddr_in server_addr;//服务器端地址
+    int addrlen=sizeof(server_addr);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);//服务器端口号定为6666
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    bind(sockSrv,(SOCKADDR*)&server_addr,sizeof(SOCKADDR));//套接字绑定服务器端地址
+    listen(sockSrv,5);//队列的最大长度为5
 
-	//服务器需要将各客户端的端口号存起来，和客户的名字形成一个映射，才能进行转发
-	//所以先问客户叫什么名字
-	int len;
-	printf("监听端口：%d\n",SERVER_PORT);
-	int i=0;
-	while(1){
-		struct sockaddr_in addrClient;//客户端地址
-		SOCKET client = accept(sockSrv, (SOCKADDR*)&addrClient, &addrlen);//监听到了用户，创建一个socket
-		cout<<"当前客户的端口号："<<addrClient.sin_port<<endl;//输出当前客户的端口号
+    //服务器需要将各客户端的端口号存起来，和客户的名字形成一个映射，才能进行转发
+    //所以先问客户叫什么名字
+    int len;
+    printf("监听端口：%d\n",SERVER_PORT);
+    int i=0;
+    while(1){
+        struct sockaddr_in addrClient;//客户端地址
+        SOCKET client = accept(sockSrv, (SOCKADDR*)&addrClient, &addrlen);//监听到了用户，创建一个socket
+        cout<<"当前客户的端口号："<<addrClient.sin_port<<endl;//输出当前客户的端口号
 
-		
-		HANDLE hThread=CreateThread(NULL, 0, handlerRequest,LPVOID(client), 0,NULL);//给每个用户创建一个线程，lpvoid传的是指针
-		CloseHandle(hThread);
-	}
-	closesocket(sockSrv);
-	WSACleanup();
+        
+        HANDLE hThread=CreateThread(NULL, 0, handlerRequest,LPVOID(client), 0,NULL);//给每个用户创建一个线程，lpvoid传的是指针
+        CloseHandle(hThread);
+    }
+    closesocket(sockSrv);
+    WSACleanup();
 
 }
 DWORD WINAPI handlerRequest(LPVOID lpParam){
-	SOCKET client=(SOCKET)lpParam;
-	
-	string sendbuf="welcome";
-	send_to(client,sendbuf);
-			
+    SOCKET client=(SOCKET)lpParam;
+    
+    string sendbuf="welcome";
+    send_to(client,sendbuf);
+            
 
-	//接收用户名
-	string start=recv_from(client);
-	cout<<start<<"上线"<<endl;
-	name_socket[start]=client;
-	users.push_back(start);
-	sendbuf=show_online_users();
-	//向所有人发送当前人数
-	for(user=users.begin();user!=users.end();++user){
-		send_to(name_socket[*user],sendbuf);
-	}
-	
-	//接收聊天对象
-	string endx;
-	
-	while(1){
-		string buffer=recv_from(client);//收到要转发的信息
-		if(buffer.substr(0,3)=="con"){
-			endx=buffer.substr(3,buffer.size()-3);
-			name_name[start]=endx;
-			name_name[endx]=start;//反向也强制连接 
-			cout<<start<<"申请和"<<endx<<"连接"<<endl;
-			if(!user_is_online(endx)){
-				send_to(client,"该用户未上线");
-			}
-			else{ 
-				send_to(client,"服务器收到建立连接申请");
-				send_to(name_socket[endx],"invite$"+start+"$邀请你聊天");
-			}
-			continue;
-		}
-		cout<<start<<"->"<<name_name[start]<<":"<<buffer<<endl;
-		//正常转发信息 
-		send_to(name_socket[name_name[start]],buffer);
-		//如果其中一个用户退出了，要让另一个用户重新选择用户聊天
-		if(buffer=="quit"){
-			users.remove(start);
-			cout<<start<<"已下线"<<endl;
-			string sendbuf=show_online_users();
-			for(user=users.begin();user!=users.end();++user){
-				send_to(name_socket[*user],sendbuf);
-			}
-			return 0;
-		}
-	}
-	
-	return 0;
+    //接收用户名
+    string start=recv_from(client);
+    cout<<start<<"上线"<<endl;
+    name_socket[start]=client;
+    users.push_back(start);
+    sendbuf=show_online_users();
+    //向所有人发送当前人数
+    for(user=users.begin();user!=users.end();++user){
+        send_to(name_socket[*user],sendbuf);
+    }
+    
+    //接收聊天对象
+    string endx;
+    
+    while(1){
+        string buffer=recv_from(client);//收到要转发的信息
+        if(buffer.substr(0,3)=="con"){
+            endx=buffer.substr(3,buffer.size()-3);
+            name_name[start]=endx;
+            name_name[endx]=start;//反向也强制连接 
+            cout<<start<<"申请和"<<endx<<"连接"<<endl;
+            if(!user_is_online(endx)){
+                send_to(client,"该用户未上线");
+            }
+            else{ 
+                send_to(client,"服务器收到建立连接申请");
+                send_to(name_socket[endx],"invite$"+start+"$邀请你聊天");
+            }
+            continue;
+        }
+        cout<<start<<"->"<<name_name[start]<<":"<<buffer<<endl;
+        //正常转发信息 
+        send_to(name_socket[name_name[start]],buffer);
+        //如果其中一个用户退出了，要让另一个用户重新选择用户聊天
+        if(buffer=="quit"){
+            users.remove(start);
+            cout<<start<<"已下线"<<endl;
+            string sendbuf=show_online_users();
+            for(user=users.begin();user!=users.end();++user){
+                send_to(name_socket[*user],sendbuf);
+            }
+            return 0;
+        }
+    }
+    
+    return 0;
 }
